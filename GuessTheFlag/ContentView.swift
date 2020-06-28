d0fdc13ae9596b005fff46e3f0e50b7bcd0af832
marvinhm <marvinmatovu@gmail.com>
2020-06-28 23:44:05 +0100

    Styling our flags

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -33,13 +33,16 @@ struct ContentView: View {
 
     var body: some View {
         ZStack {
-            Color.blue.edgesIgnoringSafeArea(.all)
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+            .edgesIgnoringSafeArea(.all)
             VStack(spacing: 30) {
                 VStack {
                     Text("Tap the flag of")
                         .foregroundColor(.white)
                     Text(countries[self.correctAnswer])
                     .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .fontWeight(.black)
                 }
                 
                 ForEach(0 ..< 3) { number in
@@ -48,6 +51,9 @@ struct ContentView: View {
                     }) {
                         Image(self.countries[number])
                         .renderingMode(.original)
+                        .clipShape(Capsule())
+                        .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                        .shadow(color: .black, radius: 2)
                     }
                 }
                 Spacer()
